b5db5aab52ce869d956394ec39ec628001da7222
KareemMusleh <prototribe@protonmail.com>
2025-03-24 08:59:32 +0700

    add searchGithubRepo and make it multithreaded

--- main.go
+++ main.go
@@ -9,13 +9,20 @@ import (
 	"net/url"
 	"os"
 	"path/filepath"
-	"time"
-	"strings"
 	"regexp"
+	"strings"
+	"sync"
+	"time"
 
 	git "github.com/go-git/go-git/v5"
 	"github.com/go-git/go-git/v5/plumbing"
 )
+
+var BBOUNTY_PATH string = "/hdd/home/me/bbounty/"
+var GITHUB_TOKEN string = os.Getenv("GITHUB_TOKEN")
+var HACKERONE_TOKEN string = os.Getenv("HACKERONE_TOKEN")
+var HACKERONE_USERNAME string = "zxcv_enjoyer"
+var TRACKER_DIR string = ".GITHUB_STALKER"
 
 type Repository struct {
 	Name     string `json:"name"`
@@ -23,36 +30,30 @@ type Repository struct {
 	Fork     bool   `json:"fork"`
 }
 
-var BBOUNTY_PATH string = "/home/me/bbounty/"
-var GITHUB_TOKEN string = os.Getenv("GITHUB_TOKEN")
-var HACKERONE_TOKEN string = os.Getenv("HACKERONE_TOKEN")
-var HACKERONE_USERNAME string = "zxcv_enjoyer"
-var trackerDirName string = ".GITHUB_TRACKER"
-
 type StructuredScope struct {
-	ID   string `json:"id"`
-	Type string `json:"type"`
+	ID         string `json:"id"`
+	Type       string `json:"type"`
 	Attributes struct {
-		AssetType          string    `json:"asset_type"`
-		AssetIdentifier    string    `json:"asset_identifier"`
-		EligibleForBounty  bool      `json:"eligible_for_bounty"`
-		EligibleForSubmission bool   `json:"eligible_for_submission"`
-		Instruction        *string   `json:"instruction"`
-		MaxSeverity        string    `json:"max_severity"`
-		CreatedAt          time.Time `json:"created_at"`
-		UpdatedAt          time.Time `json:"updated_at"`
-		ConfidentialityRequirement *string `json:"confidentiality_requirement,omitempty"`
-		IntegrityRequirement       *string `json:"integrity_requirement,omitempty"`
-		AvailabilityRequirement    *string `json:"availability_requirement,omitempty"`
+		AssetType                  string    `json:"asset_type"`
+		AssetIdentifier            string    `json:"asset_identifier"`
+		EligibleForBounty          bool      `json:"eligible_for_bounty"`
+		EligibleForSubmission      bool      `json:"eligible_for_submission"`
+		Instruction                *string   `json:"instruction"`
+		MaxSeverity                string    `json:"max_severity"`
+		CreatedAt                  time.Time `json:"created_at"`
+		UpdatedAt                  time.Time `json:"updated_at"`
+		ConfidentialityRequirement *string   `json:"confidentiality_requirement,omitempty"`
+		IntegrityRequirement       *string   `json:"integrity_requirement,omitempty"`
+		AvailabilityRequirement    *string   `json:"availability_requirement,omitempty"`
 	} `json:"attributes"`
 }
 
 type Links struct {
 	Self  string `json:"self"`
-	Next string `json:"next"`
+	Next  string `json:"next"`
 	Prev  string `json:"prev"`
 	First string `json:"first"`
-	Last string `json:"last"`
+	Last  string `json:"last"`
 }
 
 type Response struct {
@@ -60,11 +61,10 @@ type Response struct {
 	Links Links             `json:"links"`
 }
 
-
-// curl "https://api.hackerone.com/v1/programs/{id}/structured_scopes" \
-//   -X GET \
-//   -u "<YOUR_API_USERNAME>:<YOUR_API_TOKEN>" \
-//   -H 'Accept: application/json'
+//	curl "https://api.hackerone.com/v1/programs/{id}/structured_scopes" \
+//	  -X GET \
+//	  -u "<YOUR_API_USERNAME>:<YOUR_API_TOKEN>" \
+//	  -H 'Accept: application/json'
 func GetWildcards(programName string) ([]string, error) {
 	var wildcards []string
 	apiURL := fmt.Sprintf("https://api.hackerone.com/v1/hackers/programs/%s/structured_scopes", programName)
@@ -76,17 +76,14 @@ func GetWildcards(programName string) ([]string, error) {
 	req.Header.Add("Accept", "application/json")
 	req.Header.Add("User-Agent", "GOLANG")
 	client := &http.Client{}
-	if err != nil {
-		return nil, fmt.Errorf("failed to fetch program details: %v", err)
-	}
 
 	for {
 		req.URL, err = url.ParseRequestURI(apiURL)
-		if (err != nil) {
+		if err != nil {
 			return nil, fmt.Errorf("failed to parse request url %s: %v", apiURL, err)
 		}
 		resp, err := client.Do(req)
-		if (err != nil) {
+		if err != nil {
 			return nil, fmt.Errorf("failed to send request: %v", err)
 		}
 		defer resp.Body.Close()
@@ -112,7 +109,7 @@ func GetWildcards(programName string) ([]string, error) {
 				wildcards = append(wildcards, scope.Attributes.AssetIdentifier)
 			}
 		}
-		if (program.Links.Next == "") {
+		if program.Links.Next == "" {
 			break
 		} else {
 			apiURL = program.Links.Next
@@ -120,19 +117,19 @@ func GetWildcards(programName string) ([]string, error) {
 	}
 	return wildcards, nil
 }
-func CreateWildcardsRegexps(wildcards []string) ([]string, error) {
-	// The returned regexps divide the URI into 4 parts
+func CreateWildcardsRegexes(wildcards []string) ([]string, error) {
+	// The returned regexs divide the URI into 4 parts
 	// protocol, subdomain, port, directory
-	var regexps []string
+	var regexes []string
 	for _, wildcard := range wildcards {
-		if (wildcard[:2] != "*." || strings.Count(wildcard, "*") != 1) {
+		if wildcard[:2] != "*." || strings.Count(wildcard, "*") != 1 {
 			return nil, fmt.Errorf("non-standard wildcard %v", wildcard)
 		}
-		wildcard = strings.ReplaceAll(pattern, ".", "\.")
-		regexp := fmt.Sprintf("(?:(\S+)://)?((?:[a-zA-Z0-9._%+-]+\\.)?%s)(?::?(\\d+))?(/[/a-zA-Z0-9._%+-]*)?", wildcard[2:])
-		regexps = append(regexps, regexp)
+		wildcard = strings.ReplaceAll(wildcard, ".", `\.`)
+		regex := fmt.Sprintf("(?:(\\w+)://)?((?:[a-zA-Z0-9._%%+-]+)?%s)(?::?(\\d+))?(/[/a-zA-Z0-9._%%+-]*)?", wildcard[3:])
+		regexes = append(regexes, regex)
 	}
-	return regexps, nil
+	return regexes, nil
 }
 func fileExists(path string) bool {
 	_, err := os.Stat(path)
@@ -159,7 +156,7 @@ func GetGithubRepos(username string, perPage, pageIndex int) (*http.Response, er
 	}
 	client := &http.Client{}
 	resp, err := client.Do(req)
-	if (err != nil) {
+	if err != nil {
 		return nil, fmt.Errorf("failed to send request: %v", err)
 	}
 	defer resp.Body.Close()
@@ -177,7 +174,7 @@ func InitGithubClone(username string, initForks bool, options ...int) error {
 	} else {
 		maxTries = 10
 	}
-	userDir := GetUserDir(username)
+	userDir := GetUserPath(username)
 	pageIndex := 1
 	for {
 		fmt.Printf("Looking at page %v\n", pageIndex)
@@ -258,6 +255,7 @@ func ReadBlob(repoDir, objectID string) ([]byte, error) {
 }
 
 // Return a list of all blobs that correspond to a particular repo dir
+// Probaby will have to replace this later with a smarter method
 func GetRepoBlobs(repoDir string) ([]string, error) {
 	repo, err := git.PlainOpen(repoDir)
 	var repoBlobs []string
@@ -301,11 +299,63 @@ func GetRepoBlobs(repoDir string) ([]string, error) {
 func defaultError(err error) {
 	log.Fatalf("Error: %v", err)
 }
-func GetUserDir(username string) string {
-	return filepath.Join(BBOUNTY_PATH, username, "github")
+func GetUserPath(user string) string {
+	return filepath.Join(BBOUNTY_PATH, user, "github")
 }
+
+func GetTrackerPath(user string, repo string) string {
+	return filepath.Join(BBOUNTY_PATH, user, "github", repo, TRACKER_DIR)
+}
+
+func searchGithubRepo(githubUser string, githubRepo string, regexes []string) ([]string, error) {
+	var results []string
+	trackerPath := GetTrackerPath(githubUser, githubRepo)
+	blobs, err := os.ReadDir(trackerPath)
+	fmt.Println(trackerPath)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read dir %s: %v", trackerPath, err)
+	}
+	var wg sync.WaitGroup
+	resultsChan := make(chan []string, len(blobs))
+	var mu sync.Mutex
+	for _, blob := range blobs {
+		if !blob.IsDir() {
+			wg.Add(1)
+			go func(blob os.DirEntry) {
+				defer wg.Done()
+				filePath := filepath.Join(trackerPath, blob.Name())
+				content, err := os.ReadFile(filePath)
+				if err != nil {
+					fmt.Printf("failed to read file %s: %v\n", filePath, err)
+					return
+				}
+				contentStr := string(content)
+				var localMatches []string
+				for _, regex := range regexes {
+					re := regexp.MustCompile(regex)
+					matches := re.FindAllString(contentStr, -1)
+					localMatches = append(localMatches, matches...)
+				}
+				if len(localMatches) > 0 {
+					resultsChan <- localMatches
+				}
+			}(blob)
+		}
+	}
+	go func() {
+		wg.Wait()
+		close(resultsChan)
+	}()
+	for matches := range resultsChan {
+		mu.Lock()
+		results = append(results, matches...)
+		mu.Unlock()
+	}
+	return results, nil
+}
+
 func TrackUser(username string) error {
-	userDir := GetUserDir(username)
+	userDir := GetUserPath(username)
 	gitRepos, err := os.ReadDir(userDir)
 	if err != nil {
 		return fmt.Errorf("failed to read dir %s: %v", userDir, err)
@@ -320,7 +370,7 @@ func TrackUser(username string) error {
 		if err != nil {
 			return fmt.Errorf("failed to get repo blobs for %s: %v", repoDir, err)
 		}
-		currTrackerDir := filepath.Join(userDir, gitRepo.Name(), trackerDirName)
+		currTrackerDir := filepath.Join(userDir, gitRepo.Name(), TRACKER_DIR)
 		err = os.Mkdir(currTrackerDir, 0777)
 		if err != nil && !os.IsExist(err) {
 			return fmt.Errorf("failed to create a directory: %v", err)
@@ -341,47 +391,19 @@ func TrackUser(username string) error {
 	}
 	return nil
 }
-// func main() {
-// 	username := "roblox"
-// 	if err := InitGithubClone(username, false); err != nil {
-// 		defaultError(err)
-// 	}
-// 	err := TrackUser(username)
-// 	if err != nil {
-// 		defaultError(err)
-// 	}
-// 	// pattern := `\b[a-zA-Z0-9._%+-]+\.snooguts\.net\b`
-// 	// SearchUser(username, pattern)
-// }
 
 func main() {
-	// programName := "reddit"
-	// domains, err := GetWildcardDomains(programName)
-	// if err != nil {
-		// fmt.Println("Error:", err)
-		// return
-	// }
-	// fmt.Println("Wildcard Domains:", domains)
-	// wildcard := "*.redditinc.com"
-	// pattern := strings.ReplaceAll(wildcard, "*", "[a-zA-Z0-9._%+-]+")
-	// pattern := strings.ReplaceAll(wildcard, ".", "\\.")
-	if (wildcard[:2] != "*." || strings.Count(wildcard, "*") != 1) {
-		return nil, fmt.Errorf("non-standard wildcard %v", wildcard)
+	hackeroneName := "reddit"
+	githubName := "reddit"
+	wildcards, err := GetWildcards(hackeroneName)
+	if err != nil {
+		defaultError(err)
 	}
-	pattern := "(?:(https?)://)?((?:[a-zA-Z0-9._%+-]+\\.)?redditinc.com)(?::?(\\d+))?(/[/a-zA-Z0-9._%+-]*)?"
-	pattern = strings.ReplaceAll(pattern, "", "")
-	re := regexp.MustCompile(pattern)
-	testStrings := []string{
-		"www.redditinc.com/asdf",
-		"sub.redditinc.com:8000",
-		"redditinc.com",
-		"https://www.redditinc.com:8443/asdf/asdf.php",
-		"otherdomain.com",
+	regexes, _ := CreateWildcardsRegexes(wildcards)
+	fmt.Println(regexes)
+	results, err := searchGithubRepo(githubName, "devvit", regexes)
+	if err != nil {
+		defaultError(err)
 	}
-	for _, test := range testStrings {
-		match := re.FindStringSubmatch(test)
-		if len(match) != 0 {
-		fmt.Printf("Match %s: %v %v\n", test, match, len(match))
-		}
-	}
+	fmt.Println(results)
 }
